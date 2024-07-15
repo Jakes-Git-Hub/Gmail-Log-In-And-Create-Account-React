ccb63bfba05ca95857092d7b01ab35ac225d66a8
Jacob Matthews <jacmatthews7@gmail.com>
2024-07-15 09:34:55 +0100

    Cleaned Code and Implemented AWS secret retrieval for Google API Key

--- src/utils/APIEndPointLimiter.js
+++ src/utils/APIEndPointLimiter.js
@@ -1,30 +1,21 @@
 import RateLimit from 'axios-rate-limit';
 import axios from 'axios';
 
-const fetchSecrets = async () => {
+const fetchApiBaseUrl = async () => {
   try {
     const response = await fetch('http://localhost:3001/api/secrets');
     const data = await response.json();
-    return data;
+    const baseUrl = data.REACT_APP_API_BASE_URL_LOCAL;
+    console.log('baseUrl:', baseUrl); // For debugging
+    return baseUrl;
   } catch (error) {
-    console.error('Error fetching secrets:', error);
-    throw error; // Re-throw the error for proper handling
-  }
-};
-
-const getApiBaseUrl = async () => {
-  try {
-    const data = await fetchSecrets();
-    console.log('data.REACT_APP_API_BASE_URL_LOCAL', data.REACT_APP_API_BASE_URL_LOCAL);
-    return data.REACT_APP_API_BASE_URL_LOCAL;
-  } catch (error) {
-    console.error('Error getting API base URL:', error);
+    console.error('Error fetching API base URL:', error);
     throw error; // Re-throw the error for proper handling
   }
 };
 
 export const APIEndPointLimiter = async (maxRequests, windowMs) => {
-  const baseURL = await getApiBaseUrl();
+  const baseURL = await fetchApiBaseUrl();
   const axiosInstance = axios.create({ baseURL });
   const limiter = RateLimit(axiosInstance, { maxRequests, windowMs });
   return limiter;
